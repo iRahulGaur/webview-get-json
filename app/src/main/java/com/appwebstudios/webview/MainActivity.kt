65dcd1fe5bf43b4e354bbc89202745b3b3b8d840
Rahul Gaur <rahul.gaur152@gmail.com>
2021-08-16 09:57:28 +0530

    converted to kotlin

--- app/src/main/java/com/appwebstudios/webview/MainActivity.kt
+++ app/src/main/java/com/appwebstudios/webview/MainActivity.kt
@@ -1,139 +1,124 @@
-package com.appwebstudios.webview;
+package com.appwebstudios.webview
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.webkit.ConsoleMessage;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import org.apache.commons.io.IOUtils;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.URL;
-import java.net.URLConnection;
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import com.appwebstudios.webview.R
+import com.appwebstudios.webview.MainActivity
+import org.json.JSONObject
+import androidx.annotation.RequiresApi
+import android.os.Build
+import android.content.Intent
+import android.app.Activity
+import android.util.Log
+import android.webkit.*
+import org.apache.commons.io.IOUtils
+import java.io.StringWriter
+import java.lang.Exception
+import java.net.URL
 
 /*
  *@aurthur Rahul Gaur
  *@date 14.11.19
  */
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    private static final String TAG = "MainActivity";
-    private boolean flag = false;
-    private int checker = 0;
-    private int result = 0;
+    private var flag = false
+    private var checker = 0
+
+    private var result = 0
 
     @SuppressLint("SetTextI18n")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        WebView mWebView = findViewById(R.id.webView);
-
-        mWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-                Log.e(TAG, "onConsoleMessage: console " + consoleMessage.message());
-                return true;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mWebView = findViewById<WebView>(R.id.webView)
+        mWebView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                Log.e(TAG, "onConsoleMessage: console " + consoleMessage.message())
+                return true
             }
-        });
-
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Nullable
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
-
+        }
+        mWebView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                val url = request.url.toString()
                 if (flag && checker == 0) {
-                    checker = 1;
+                    checker = 1
                     try {
-                        URL aURL = new URL(url);
-                        URLConnection conn = aURL.openConnection();
-                        conn.connect();
-                        InputStream is = conn.getInputStream();
-
-                        StringWriter writer = new StringWriter();
-                        IOUtils.copy(is, writer, "UTF-8");
-                        Log.e(TAG, "shouldInterceptRequest: string " + writer.toString());
-
-                        JSONObject object = new JSONObject(writer.toString());
-                        Log.e(TAG, "shouldInterceptRequest: json object " + object);
-
-                        if (object.getString("completed").contains("false")) {
-                            result = 1;
+                        val aURL = URL(url)
+                        val conn = aURL.openConnection()
+                        conn.connect()
+                        val `is` = conn.getInputStream()
+                        val writer = StringWriter()
+                        IOUtils.copy(`is`, writer, "UTF-8")
+                        Log.e(TAG, "shouldInterceptRequest: string $writer")
+                        val `object` = JSONObject(writer.toString())
+                        Log.e(TAG, "shouldInterceptRequest: json object $`object`")
+                        result = if (`object`.getString("completed").contains("false")) {
+                            1
                         } else {
-                            result = 2;
+                            2
                         }
-
-                        return super.shouldInterceptRequest(view, request);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Log.e(TAG, "shouldInterceptRequest: exception " + e.getMessage());
+                        return super.shouldInterceptRequest(view, request)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Log.e(TAG, "shouldInterceptRequest: exception " + e.message)
                     }
                 }
-                return null;
+                return null
             }
 
             @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                Log.e(TAG, "onReceivedError: error " + error.getDescription());
+            override fun onReceivedError(
+                view: WebView,
+                request: WebResourceRequest,
+                error: WebResourceError
+            ) {
+                Log.e(TAG, "onReceivedError: error " + error.description)
             }
 
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                Log.e(TAG, "onPageFinished: finish " + url);
+            override fun onPageFinished(view: WebView, url: String) {
+                Log.e(TAG, "onPageFinished: finish $url")
                 if (url.contains("/></head><body>")) {
-                    flag = true;
+                    flag = true
                 }
-
                 if (result == 1) {
-                    finishSuccessActivity();
+                    finishSuccessActivity()
                 } else if (result == 2) {
-                    finishFailedActivity();
+                    finishFailedActivity()
                 }
             }
-        });
-
-        String content =
-                "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
-                        "<html><head>" +
-                        "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />" +
-                        "</head><body><form method=\"get\" action=\"https://jsonplaceholder.typicode.com/todos/1\"><button type=\"submit\">Continue</button></form></body></html>";
+        }
+        val content = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" +
+                "<html><head>" +
+                "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />" +
+                "</head><body><form method=\"get\" action=\"https://jsonplaceholder.typicode.com/todos/1\"><button type=\"submit\">Continue</button></form></body></html>"
 
         //<html><head></head><body><a href=\"URL\">get emp data</a></body></html>
 
         //<a href="https://jsonplaceholder.typicode.com/todos/1">get emp data</a>
-        mWebView.loadData(content, "text/html; charset=utf-8", "UTF-8");
+        mWebView.loadData(content, "text/html; charset=utf-8", "UTF-8")
     }
 
-    void finishSuccessActivity() {
-        Log.e(TAG, "finishSuccessActivity: called finish success ");
-        Intent returnIntent = new Intent();
-        returnIntent.putExtra("result", "False");
-        setResult(Activity.RESULT_OK, returnIntent);
-        finish();
+    fun finishSuccessActivity() {
+        Log.e(TAG, "finishSuccessActivity: called finish success ")
+        val returnIntent = Intent()
+        returnIntent.putExtra("result", "False")
+        setResult(RESULT_OK, returnIntent)
+        finish()
     }
 
-    void finishFailedActivity() {
-        Log.e(TAG, "finishFailedActivity: called failed ");
-        Intent returnIntent = new Intent();
-        setResult(Activity.RESULT_CANCELED, returnIntent);
-        finish();
+    fun finishFailedActivity() {
+        Log.e(TAG, "finishFailedActivity: called failed ")
+        val returnIntent = Intent()
+        setResult(RESULT_CANCELED, returnIntent)
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/appwebstudios/webview/Welcome.kt
+++ app/src/main/java/com/appwebstudios/webview/Welcome.kt
@@ -1,53 +1,48 @@
-package com.appwebstudios.webview;
+package com.appwebstudios.webview
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+class Welcome : AppCompatActivity() {
 
-public class Welcome extends AppCompatActivity {
+    companion object {
+        private const val TAG = "Welcome"
+        private const val SUCCESS_RESPONSE = 111
+    }
 
-    private TextView statusTV;
-    private static final String TAG = "Welcome";
+    private var statusTV: TextView? = null
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-
-        statusTV = findViewById(R.id.statusView);
-        Button webBtn = findViewById(R.id.webBtn);
-
-        webBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivityForResult(i, 1);
-            }
-        });
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+        statusTV = findViewById(R.id.statusView)
+        val webBtn = findViewById<Button>(R.id.webBtn)
+        webBtn.setOnClickListener { v: View? ->
+            val i = Intent(applicationContext, MainActivity::class.java)
+            startActivityForResult(i, SUCCESS_RESPONSE)
+        }
     }
 
     @SuppressLint("SetTextI18n")
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1) {
-            if (resultCode == Activity.RESULT_OK) {
+            if (resultCode == RESULT_OK) {
                 //result is ok
-                Log.e(TAG, "onActivityResult: everything worked fine ");
-                statusTV.setText("Status: Success");
-            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.e(TAG, "onActivityResult: everything worked fine ")
+                statusTV!!.text = "Status: Success"
+            } else if (resultCode == RESULT_CANCELED) {
                 //result is not ok
-                Log.e(TAG, "onActivityResult: something wrong ");
-                statusTV.setText("Status: Failed");
+                Log.e(TAG, "onActivityResult: something wrong ")
+                statusTV!!.text = "Status: Failed"
             }
         }
     }
+
 }
